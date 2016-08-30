b3a702807ef2a053e3b32b50c8179c4c8f9dc78b
dingcang <15538928689@163.com>
2016-08-30 17:20:54 +0800

    html

---
+++ html/分页/分页1/js1/addHtml.js
@@ -0,0 +1,8 @@
+$(function(){
+	//var data=["0.jpg","1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg","9.jpg","10.jpg","11.jpg","12.jpg","13.jpg","14.jpg","15.jpg","16.jpg","17.jpg","18.jpg","19.jpg","20.jpg","21.jpg"];
+	var li="";
+	for(var i=0;i<40;i++){
+		li+="<tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td><td>8</td><td>9</td></tr>";
+	}
+	$("#itemContainer").append(li);
+});
